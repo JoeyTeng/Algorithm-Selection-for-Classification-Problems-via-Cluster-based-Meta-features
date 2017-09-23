eefb0520f0d484d559393936ad6922621ff7a1e6
Joey Teng <joey.teng.dev@gmail.com>
2017-09-23 14:07:05 +0800

    Update exam.py

--- exam.py
+++ exam.py
@@ -3,8 +3,7 @@
 # @Email:  joey.teng.dev@gmail.com
 # @Filename: exam.py
 # @Last modified by:   Toujour
-# @Last modified time: 14-Jun-2017
-
+# @Last modified time: 18-Jul-2017
 
 
 from sys import argv
@@ -18,6 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 from scipy import stats
 import numpy as np
 import json
+
 
 class Models(object):
     def __init__(self):
@@ -33,7 +33,8 @@ class Models(object):
         def score(self, X, y, sample_weight=None):
             self.set_params(n_neighbors=len(X))
             return self._score__(X, y, sample_weight)
-        model = KNeighborsClassifier(n_neighbors=len(X), weights='distance', n_jobs=-1)
+        model = KNeighborsClassifier(
+            n_neighbors=len(X), weights='distance', n_jobs=-1)
         KNeighborsClassifier._score__, KNeighborsClassifier.score = KNeighborsClassifier.score, score
 
         return ('K-Nearest Neighbors (hacked, All neighbors)', model)
@@ -47,6 +48,7 @@ class Models(object):
     def random_forest(X, y):
         model = RandomForestClassifier()
         return ('Support Vector Machine', model)
+
 
 class MetaFeature(object):
     def __init__(self, X, y):
@@ -66,28 +68,28 @@ class MetaFeature(object):
         """
         Number of examples
         """
-        return self.__universal_setter__('N', lambda:len(self._y))
+        return self.__universal_setter__('N', lambda: len(self.y))
 
     @property
     def p(self):
         """
         Number of attributes
         """
-        return self.__universal_setter__('p', lambda:len(self._X[0]))
+        return self.__universal_setter__('p', lambda: len(self._X[0]))
 
     @property
     def q(self):
         """
         Number of classes
         """
-        return self.__universal_setter__('q', lambda:len(np.unique(self._y)))
+        return self.__universal_setter__('q', lambda: len(np.unique(self.y)))
 
     @property
     def Bin_att(self):
         """
         Number of binary attributes
         """
-        return self.__universal_setter__('Bin.att', lambda:len([i for i in np.transpose(self._X) if len(np.unique(i)) <= 2]))
+        return self.__universal_setter__('Bin.att', lambda: len([i for i in np.transpose(self._X) if len(np.unique(i)) <= 2]))
 
     @property
     def cost(self):
@@ -113,6 +115,11 @@ class MetaFeature(object):
         TODO:
 
         Mean absolute correlation of attributes
+
+        Only applicable for continuous classes.
+        Mean abs(R value) of the attributes
+        Relation between the dependent var and the set of independent vars
+            normalized error.
         """
         pass
 
@@ -139,14 +146,14 @@ class MetaFeature(object):
         r"""
         Mean of $\E (X - \mu)^3\/\sigma^3$
         """
-        return self.__universal_setter__('skewness', lambda:stats.skew(self._X, bias=False)) # bias correction enabled. CHECK!
+        return self.__universal_setter__('skewness', lambda: stats.skew(self._X, bias=False))  # bias correction enabled. CHECK!
 
     @property
     def kurtosis(self):
         r"""
         Mean of $\E(X - \mu)^4\/\sigma^4$
         """
-        return self.__universal_setter__('kurtosis', lambda:stats.kurtosis(self._X, fisher=True, bias=False)) # fisher = true (normal = 0), bias correction enabled. CHECK!
+        return self.__universal_setter__('kurtosis', lambda: stats.kurtosis(self.X, fisher=True, bias=False))  # fisher = true (normal = 0), bias correction enabled. CHECK!
 
     @property
     def H_C(self):
@@ -192,9 +199,10 @@ class MetaFeature(object):
         r"""
         TODO:
 
-        Noise-signal ratio (\bar H  (X) − \bar M (C, X))/\bar M (C, X)
+        Noise-signal ratio (\bar H  (X) ? \bar M (C, X))/\bar M (C, X)
         """
         pass
+
 
 def main(fname):
     dataset = np.loadtxt(fname=fname, dtype=float, delimiter=',')
@@ -212,7 +220,8 @@ def main(fname):
 
     paired_t_test(scores)
 
-    meta_feature(X, y)
+    # meta_feature(X, y)
+
 
 def meta_feature(X, y):
     feature = MetaFeature(X, y)
@@ -221,17 +230,21 @@ def meta_feature(X, y):
             print(func)
             print(getattr(feature, func))
 
+
 def paired_t_test(scores):
-    print("paired t-test %r" %(scores.keys()))
-    result = list(map((lambda x:[stats.ttest_rel(x, b) for b in scores.values()]), scores.values()))
-    p_value = np.array(([x[1] for x in row] for row in result))
+    print("paired t-test %r" % (scores.keys()))
+    result = list(map((lambda x: [stats.ttest_rel(x, b)
+                                  for b in scores.values()]), scores.values()))
+    p_value = np.array([[x[1] for x in row] for row in result])
     print(p_value)
     # print(result)
     return result
 
+
 def preprocess(X):
     normalized_X = preprocessing.normalize(X)
     return normalized_X
+
 
 def report(model, X, y, description):
     scores = model_selection.cross_val_score(model, X, y, cv=10, n_jobs=-1)
@@ -241,8 +254,9 @@ def report(model, X, y, description):
 
     return scores
 
+
 if __name__ == '__main__':
-#    fname = json.load(argv[1])
-#    for entry in fname:
-#        main(entry[0])
+    #    fname = json.load(argv[1])
+    #    for entry in fname:
+    #        main(entry[0])
     main(argv[1])
